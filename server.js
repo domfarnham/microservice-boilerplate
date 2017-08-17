2e3d3f65382fccecffa93833e3f145befbd98fcc
System <none>
2017-08-17 15:02:17 +0000

    Reformatted to comply with JavaScript Standard Style

--- server.js
+++ server.js
@@ -1,42 +1,38 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const express = require('express');
-const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const databaseURL = "";
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 
 // Connect to database first
-MongoClient.connect(databaseURL, function(err, db) {
-  assert.equal(null, err);
-  console.log("Successfully connected to MongoDB.");
-  
+MongoClient.connect(process.env.DATABASE_URL, function (err, db) {
+  assert.equal(null, err)
+  console.log('Successfully connected to MongoDB.')
+
   // Routes
-  app.use('/public', express.static(process.cwd() + '/public'));
-  
-  app.get('/', (function(req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  }));
+  app.use('/public', express.static(process.cwd() + '/public'))
+
+  app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html')
+  })
 
   // Respond not found to all the wrong routes
-  app.use(function(req, res, next){
-    res.status(404);
-    res.type('txt').send('Not found');
-  });
+  app.use(function (req, res, next) {
+    res.status(404)
+    res.type('txt').send('Not found')
+  })
 
   // Error Middleware
-  app.use(function(err, req, res, next) {
-    if(err) {
+  app.use(function (err, req, res, next) {
+    if (err) {
       res.status(err.status || 500)
         .type('txt')
-        .send(err.message || 'SERVER ERROR');
-    }  
+        .send(err.message || 'SERVER ERROR')
+    }
   })
 
   app.listen(process.env.PORT, function () {
-    console.log('Node.js listening on port ' + process.env.PORT);
-  });
-  
-});
-
+    console.log('Node.js listening on port ' + process.env.PORT)
+  })
+})
